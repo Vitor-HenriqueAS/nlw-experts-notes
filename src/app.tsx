e9ad152f3a9abb01b23c01c9dccbc0f9e2f7ba5a
Vitor-HenriqueAS <vitorhenrique.a12@gmail.com>
2024-02-07 09:33:54 -0300

    aula 02

--- src/app.tsx
+++ src/app.tsx
@@ -22,12 +22,11 @@ export function App() {
 
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={{
+            date: new Date(),
+            content: 'Hello World'
+          }}
+        />
 
       </div>
 
